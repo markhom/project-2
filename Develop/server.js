f1a39db210944bb2ff38215b4ec47db6f8a951de
Mark Homchik <markhomchik4@gmail.com>
2024-04-02 19:06:01 -0500

    removing code

--- Develop/server.js
+++ Develop/server.js
@@ -14,21 +14,6 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
