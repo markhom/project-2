10f648e2a8958fa59a39c51266a9283f17321bf7
Mark Homchik <markhomchik4@gmail.com>
2024-04-04 18:48:44 -0500

    updating from crowdfunf_db to clothes_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,3 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+-- Active: 1710463940249@@127.0.0.1@3306@clothes
+DROP DATABASE IF EXISTS clothes_db;
+CREATE DATABASE clothes_db;
