cbfac78a0a0f3e9ffdf9e236fdff667cf793f87d
Kyle <schoenbergerk@hotmail.com>
2024-04-08 18:34:41 -0500

    added User relations to clothing items

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Hoodie = require('./hoodie');
 const Pants = require('./pants');
 const Sweater = require('./sweater');
 const Tshirt = require('./tshirt');
+const TeeShirt = require('./t-shirt');
 
 // Creates a relationship between User and Project model, with the User having a "has many" relationship with Project model.
 User.hasMany(Project, {
@@ -11,24 +12,45 @@ User.hasMany(Project, {
   onDelete: 'CASCADE'
 });
 
+User.hasMany(Hoodie, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Pants, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Sweater, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Tshirt, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+
 // Creates a relationship between User and Project model, with a "belongs to" relationship of the Project to the User.
 Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Hoodie.hasMany(User, {
+Hoodie.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Pants.hasMany(User, {
+Pants.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Sweater.hasMany(User, {
+Sweater.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Tshirt.hasMany(User, {
+Tshirt.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
