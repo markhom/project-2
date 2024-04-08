53eeb14d335398e22a22025737e0ac49cff26bcc
adelinM1 <marcusjorgensen@gmail.com>
2024-04-07 20:01:58 -0500

    updated auth

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,7 @@
 const withAuth = (req, res, next) => {
     // If the user is not logged in, redirect the request to the login route
     if (!req.session.logged_in) {
+      console.log("User is not authenticated");
       res.redirect('/login');
     } else {
       next();
